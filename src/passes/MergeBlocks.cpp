7cbeeadd8b6ecab017a0886aa18594ff5b03d323
loveshen23 <109837769+loveshen23@users.noreply.github.com>
2021-02-16 20:42:11 +0000

    Revert "staging: binder: Change binder mutex to rtmutex."

--- src/passes/MergeBlocks.cpp
+++ src/passes/MergeBlocks.cpp
@@ -177,4 +177,334 @@ struct BreakValueDropper : public ControlFlowWalker<BreakValueDropper> {
 
 static bool hasUnreachableChild(Block* block) {
   for (auto* test : block->list) {
-    if (test->type == Type::unreachable) 
+    if (test->type == Type::unreachable) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Checks for code after an unreachable element.
+static bool hasDeadCode(Block* block) {
+  auto& list = block->list;
+  auto size = list.size();
+  for (size_t i = 1; i < size; i++) {
+    if (list[i - 1]->type == Type::unreachable) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// core block optimizer routine
+static void optimizeBlock(Block* curr,
+                          Module* module,
+                          PassOptions& passOptions,
+                          BranchUtils::BranchSeekerCache& branchInfo) {
+  auto& list = curr->list;
+  // Main merging loop.
+  bool more = true;
+  bool changed = false;
+  while (more) {
+    more = false;
+    for (size_t i = 0; i < list.size(); i++) {
+      auto* child = list[i];
+      // The child block, if there is one.
+      Block* childBlock = child->dynCast<Block>();
+      // If we are merging an inner block of a loop, then we must not
+      // merge things before and including the name of the loop, moving
+      // those out would break things.
+      Loop* loop = nullptr;
+      // To to handle a non-block child.
+      if (!childBlock) {
+        // if we have a child that is (drop (block ..)) then we can move the
+        // drop into the block, and remove br values. this allows more merging,
+        if (auto* drop = list[i]->dynCast<Drop>()) {
+          childBlock = drop->value->dynCast<Block>();
+          if (childBlock) {
+            if (hasUnreachableChild(childBlock)) {
+              // don't move around unreachable code, as it can change types
+              // dce should have been run anyhow
+              continue;
+            }
+            if (childBlock->name.is()) {
+              Expression* expression = childBlock;
+              // check if it's ok to remove the value from all breaks to us
+              ProblemFinder finder(passOptions);
+              finder.setModule(module);
+              finder.origin = childBlock->name;
+              finder.walk(expression);
+              if (finder.found()) {
+                childBlock = nullptr;
+              } else {
+                // fix up breaks
+                BreakValueDropper fixer(passOptions, branchInfo);
+                fixer.origin = childBlock->name;
+                fixer.setModule(module);
+                fixer.walk(expression);
+              }
+            }
+            if (childBlock) {
+              // we can do it!
+              // reuse the drop, if we still need it
+              auto* last = childBlock->list.back();
+              if (last->type.isConcrete()) {
+                drop->value = last;
+                drop->finalize();
+                childBlock->list.back() = drop;
+              }
+              childBlock->finalize();
+              child = list[i] = childBlock;
+              more = true;
+              changed = true;
+            }
+          }
+        } else if ((loop = list[i]->dynCast<Loop>())) {
+          // We can merge a loop's "tail" - if the body is a block and has
+          // instructions at the end that do not branch back.
+          childBlock = loop->body->dynCast<Block>();
+          // TODO: handle (loop (loop - the bodies of loops may not be blocks
+        }
+      }
+      // If no block, we can't do anything.
+      if (!childBlock) {
+        continue;
+      }
+      auto& childList = childBlock->list;
+      auto childSize = childList.size();
+      if (childSize == 0) {
+        continue;
+      }
+      // If the child has items after an unreachable, ignore it - dce should
+      // have been run, and we prefer to not handle the complexity here.
+      if (hasDeadCode(childBlock)) {
+        continue;
+      }
+      // In some cases we can remove only the head or the tail of the block,
+      // and must keep some things in the child block.
+      Index keepStart = childSize;
+      Index keepEnd = 0;
+      // For a block with a name, we may only be able to remove a head, up
+      // to the first item that branches to the block.
+      if (childBlock->name.is()) {
+        // If it has a concrete value, then breaks may be sending it a value,
+        // and we'd need to handle that. TODO
+        if (childBlock->type.isConcrete()) {
+          continue;
+        }
+        auto childName = childBlock->name;
+        for (size_t j = 0; j < childSize; j++) {
+          auto* item = childList[j];
+          if (branchInfo.hasBranch(item, childName)) {
+            // We can't remove this from the child.
+            keepStart = j;
+            keepEnd = childSize;
+            break;
+          }
+        }
+      }
+      // For a loop, we may only be able to remove a tail
+      if (loop) {
+        auto childName = loop->name;
+        for (auto j = int(childSize - 1); j >= 0; j--) {
+          auto* item = childList[j];
+          if (BranchUtils::BranchSeeker::has(item, childName)) {
+            // We can't remove this from the child.
+            keepStart = 0;
+            keepEnd = std::max(Index(j + 1), keepEnd);
+            break;
+          }
+        }
+        // If we can only do part of the block, and if the block has a flowing
+        // value, we would need special handling for that - not worth it,
+        // probably TODO
+        // FIXME is this not handled by the drop later down?
+        if (keepEnd < childSize && childList.back()->type.isConcrete()) {
+          continue;
+        }
+      }
+      // Maybe there's nothing to do, if we must keep it all in the
+      // child anyhow.
+      if (keepStart == 0 && keepEnd == childSize) {
+        continue;
+      }
+      // There is something to do!
+      bool keepingPart = keepStart < keepEnd;
+      // Create a new merged list, and fill in the code before the
+      // child block we are merging in. TODO better efficiency
+      ExpressionList merged(module->allocator);
+      for (size_t j = 0; j < i; j++) {
+        merged.push_back(list[j]);
+      }
+      // Add the head of the block - the things at the start we do
+      // not need to keep.
+      for (Index j = 0; j < keepStart; j++) {
+        merged.push_back(childList[j]);
+      }
+      // If we can't merge it all, keep and filter the child.
+      if (keepingPart) {
+        merged.push_back(child);
+        // Filter the child.
+        ExpressionList filtered(module->allocator);
+        for (Index j = keepStart; j < keepEnd; j++) {
+          filtered.push_back(childList[j]);
+        }
+        // Add the tail of the block - the things at the end we do
+        // not need to keep.
+        for (Index j = keepEnd; j < childSize; j++) {
+          merged.push_back(childList[j]);
+        }
+        // Update the child.
+        childList.swap(filtered);
+        // We may have removed unreachable items.
+        childBlock->finalize();
+        if (loop) {
+          loop->finalize();
+        }
+        // Note that we modify the child block here, which invalidates info
+        // in branchInfo. However, as we have scanned the parent, we have
+        // already forgotten the child's info, so there is nothing to do here
+        // for the child.
+        // (We also don't need to do anything for the parent - we move code
+        // from a child into the parent, but that doesn't change the total
+        // branches in the parent.)
+      }
+      // Add the rest of the parent block after the child.
+      for (size_t j = i + 1; j < list.size(); j++) {
+        merged.push_back(list[j]);
+      }
+      // if we merged a concrete element in the middle, drop it
+      if (!merged.empty()) {
+        auto* last = merged.back();
+        for (auto*& item : merged) {
+          if (item != last && item->type.isConcrete()) {
+            Builder builder(*module);
+            item = builder.makeDrop(item);
+          }
+        }
+      }
+      list.swap(merged);
+      more = true;
+      changed = true;
+      break;
+    }
+  }
+  if (changed) {
+    curr->finalize(curr->type);
+  }
+}
+
+void BreakValueDropper::visitBlock(Block* curr) {
+  optimizeBlock(curr, getModule(), passOptions, branchInfo);
+}
+
+struct MergeBlocks
+  : public WalkerPass<
+      PostWalker<MergeBlocks, UnifiedExpressionVisitor<MergeBlocks>>> {
+  bool isFunctionParallel() override { return true; }
+
+  std::unique_ptr<Pass> create() override {
+    return std::make_unique<MergeBlocks>();
+  }
+
+  BranchUtils::BranchSeekerCache branchInfo;
+
+  void visitBlock(Block* curr) {
+    optimizeBlock(curr, getModule(), getPassOptions(), branchInfo);
+  }
+
+  // given
+  // (curr
+  //  (block=child
+  //   (..more..)
+  //   (back)
+  //  )
+  //  (..other..children..)
+  // )
+  // if child is a block, we can move this around to
+  // (block
+  //  (..more..)
+  //  (curr
+  //   (back)
+  //   (..other..children..)
+  //  )
+  // )
+  // at which point the block is on the outside and potentially mergeable with
+  // an outer block
+  Block* optimize(Expression* curr,
+                  Expression*& child,
+                  Block* outer = nullptr,
+                  Expression** dependency1 = nullptr,
+                  Expression** dependency2 = nullptr) {
+    if (!child) {
+      return outer;
+    }
+    if ((dependency1 && *dependency1) || (dependency2 && *dependency2)) {
+      // there are dependencies, things we must be reordered through. make sure
+      // no problems there
+      EffectAnalyzer childEffects(getPassOptions(), *getModule(), child);
+      if (dependency1 && *dependency1 &&
+          EffectAnalyzer(getPassOptions(), *getModule(), *dependency1)
+            .invalidates(childEffects)) {
+        return outer;
+      }
+      if (dependency2 && *dependency2 &&
+          EffectAnalyzer(getPassOptions(), *getModule(), *dependency2)
+            .invalidates(childEffects)) {
+        return outer;
+      }
+    }
+    if (auto* block = child->dynCast<Block>()) {
+      if (!block->name.is() && block->list.size() >= 2) {
+        // if we move around unreachable code, type changes could occur. avoid
+        // that, as anyhow it means we should have run dce before getting here
+        if (curr->type == Type::none && hasUnreachableChild(block)) {
+          // moving the block to the outside would replace a none with an
+          // unreachable
+          return outer;
+        }
+        auto* back = block->list.back();
+        if (back->type == Type::unreachable) {
+          // curr is not reachable, dce could remove it; don't try anything
+          // fancy here
+          return outer;
+        }
+        // We are going to replace the block with the final element, so they
+        // should be identically typed. Note that we could check for subtyping
+        // here, but it would not help in the general case: we know that this
+        // block has no breaks (as confirmed above), and so the local-subtyping
+        // pass will turn its type into that of its final element, if the final
+        // element has a more specialized type. (If we did want to handle that,
+        // we'd need to then run a ReFinalize after everything, which would add
+        // more complexity here.)
+        if (block->type != back->type) {
+          return outer;
+        }
+        child = back;
+        if (outer == nullptr) {
+          // reuse the block, move it out
+          block->list.back() = curr;
+          // we want the block outside to have the same type as curr had
+          block->finalize(curr->type);
+          replaceCurrent(block);
+          return block;
+        } else {
+          // append to an existing outer block
+          assert(outer->list.back() == curr);
+          outer->list.pop_back();
+          for (Index i = 0; i < block->list.size() - 1; i++) {
+            outer->list.push_back(block->list[i]);
+          }
+          outer->list.push_back(curr);
+        }
+      }
+    }
+    return outer;
+  }
+
+  // Default optimizations for simple cases. Complex things are overridden
+  // below.
+  void visitExpression(Expression* curr) {
+    // Control flow need special handling. Those we can optimize are handled
+    // below.
+    if 
