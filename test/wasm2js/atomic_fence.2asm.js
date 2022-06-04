35a108bf13da7443681657a2dad77835a447a359
loveshen23 <109837769+loveshen23@users.noreply.github.com>
2022-06-04 16:02:23 +0000

    Updated release instructions

---
+++ test/wasm2js/atomic_fence.2asm.js
@@ -0,0 +1,19 @@
+
+function asmFunc(imports) {
+ var buffer = new ArrayBuffer(1507328);
+ var HEAP8 = new Int8Array(buffer);
+ var HEAP16 = new Int16Array(buffer);
+ var HEAP32 = new Int32Array(buffer);
+ var HEAPU8 = new Uint8Array(buffer);
+ var HEAPU16 = new Uint16Array(buffer);
+ var HEAPU32 = new Uint32Array(buffer);
+ var HEAPF32 = new Float32Array(buffer);
+ var HEAPF64 = new Float64Array(buffer);
+ var Math_imul = Math.imul;
+ var Math_fround = Math.fround;
+ var Math_abs = Math.abs;
+ var Math_clz32 = Math.clz32;
+ var Math_min = Math.min;
+ var Math_max = Math.max;
+ var Math_floor = Math.floor;
+ var Math_ceil = Math.ceil;
